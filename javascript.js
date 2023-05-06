0f050049b3dcd87d17b218067176dccaaf4fd4aa
Yontemoon <monteyoon1998@gmail.com>
2023-05-06 14:25:13 -0700

    Requires a reset button to work and grid resets as well

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 const button = document.querySelector("#button");
-
-
+const reset = document.querySelector("#reset");
+let container = document.querySelector("#container");
 let number = 0;
 
 button.addEventListener ('click', function () {
@@ -9,28 +9,35 @@ button.addEventListener ('click', function () {
     
 })
 
+reset.addEventListener ('click', function () {
+    
+    container.children.style.backgroundColor = 'white';
+    
+})
+
 container.addEventListener ('mouseover', function (e) {
     e.target.style.backgroundColor = 'black';
 });
 
+
+
+
 function createRow(width) {
 
     let number = width;
-    // let fragment = document.createDocumentFragment();
-    let container = document.querySelector("#container");
+
+
     for (let i = 1; i <= number; i++) {
         let row = document.createElement('div');
         row.classList.toggle("row");
         for (let j = 1; j <= number; j++) {
             let tile = document.createElement('div');
             tile.classList.toggle("tile");
-            console.log(tile);
-            // tile.setAttribute('id', "tileID");
             row.appendChild(tile);
         };
         container.appendChild(row);
     };
-    // container.appendChild(fragment);
+
 
 }
 
